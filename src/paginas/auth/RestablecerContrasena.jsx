f39c3fd18135e5782785cd662de0ebc13d6efd69
Sotelo Joan Sebastián <sayayinjohan4@gmail.com>
2026-03-26 17:46:49 -0500

    segundo cambio a medias jaja

--- src/paginas/auth/RestablecerContrasena.jsx
+++ src/paginas/auth/RestablecerContrasena.jsx
@@ -5,8 +5,17 @@ import { Link} from "react-router-dom";
 function RestablecerContrasena() {
     return (
         <PlantillaAuth>
-            <p className="logo-frase">"dgfbdfgdgfhdghdfhf"</p>
+            <p className="logo-frase">"Restablecer Contraseña"</p>
             <Link className="boton-volver-iniciar-sesion" to="/">Volver a iniciar sesión</Link>
+            <div className="Contenedor_Padre" id="contenedor_principal">
+                <div className="contenedor_hijo" id="contenedor_secundario">
+                    <h2>Nueva contraseña:</h2>
+                    <input type="password" placeholder="Ingrese una Contraseña"></input>
+                    <h2>Confirmar contraseña:</h2>
+                    <input type="password" placeholder="Repita la contraseña"></input>
+                    <button className="botonGuardar">Guardar nueva contraseña</button>
+                </div>
+            </div>
         </PlantillaAuth>
     );
 }
